9c688068e479da538ebff9115555271e43683d51
Aryan Sharma <aryansharma24106@gmail.com>
2025-11-26 20:33:16 +0530

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -40,7 +40,7 @@ class Product {
 
   }
 
-  extraInfoHTML(){
+  extraInfoHTML() {
 
     return '';
   }
@@ -51,14 +51,14 @@ class Clothing extends Product {
 
   sizeChartLink;
 
-  constructor(productDetails){
+  constructor(productDetails) {
 
     super(productDetails); //Calls the constructor of the parent class
     this.sizeChartLink = productDetails.sizeChartLink;
 
   }
 
-  extraInfoHTML(){
+  extraInfoHTML() {
     // super.extraInfoHTML();
     return `
     <a href="${this.sizeChartLink}" target="_blank">
@@ -76,7 +76,30 @@ console.log(date.toLocaleTimeString);
 
 */
 
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    
+    console.log('load products');
+    fun();
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -744,3 +767,5 @@ export const products = [
   }
   return new Product(productDetails);
 });
+
+*/
